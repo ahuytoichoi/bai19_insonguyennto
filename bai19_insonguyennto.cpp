d62519977e7d9f193cb4ff476ac028894d5f457f
ahuytoichoi <huy54212410028@bachkhoasaigon.edu.vn>
2024-11-25 11:17:21 +0700

    sua bai19

--- bai19_insonguyennto.cpp
+++ bai19_insonguyennto.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 int main()
 {
-	cout << "kiem tra so con phai so nguyen to khong\n";
 	int so;
 	cout << "nhap so:"; cin >> so;
 	int dem = 0;
-	for (int i = 1; i <= so; i++)
+	for (int m = 1; m <= so; m++)
 	{
-		if (so % i == 0)
+		dem = 0;
+		for (int i = 1; i <= so; i++)
 		{
-			dem++;
+			if (m % i == 0)
+			{
+				dem++;
+			}
 		}
-	}
-	cout << "sodem=" << dem << endl;
-	if (dem == 2) {
-		cout << "so" << so << "la so nguyen to\n";
-	}
-	else
-	{
-		cout << "so" << so << "khong phai la so nguyen to\n";
+		if (dem == 2)
+		{
+			cout << m << " ";
+		}
 	}
 	return 0;
 }
